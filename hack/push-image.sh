78172bac8805d04e37139f932d3e055a0608924d
Charindu Thisara <thisaraimc@gmail.com>
2024-06-21 04:31:59 +0530

    feat: Update Dockerfile and build scripts

--- hack/push-image.sh
+++ hack/push-image.sh
@@ -2,11 +2,11 @@
 set -eu
 
 echo
-echo "Please input your repo password of $1"
+# echo "Please input your repo password of $1"
 echo
-docker login --username="$1"
+# docker login --username="$1"
 
 echo
 echo "Start pushing image"
 echo
-docker images | grep "$1/ts" | awk 'BEGIN{OFS=":"}{print $1,$2}' | xargs -I {} docker push {}
+docker images | grep -v "$1/ts" | awk 'BEGIN{OFS=":"}{print $1,$2}' | xargs -I {} docker push {}
